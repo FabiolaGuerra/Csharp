cc4e6d4f6c50574a9c6ac5fd2119139145b2cbbb
FabiolaGuerra <guerrafabiola7@gmail.com>
2025-02-21 10:36:49 -0300

    Exercicio 07

---
+++ Matrizes/07 - Matrizes/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _07___Matrizes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
