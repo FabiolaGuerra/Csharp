56ed11a03b4bcd3dae7ecd29bfa7704699511f36
FabiolaGuerra <guerrafabiola7@gmail.com>
2025-03-06 11:27:59 -0300

    Primeiro ex

---
+++ POO-EX1/POO-EX1/Pedido.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_EX1
+{
+    public class Pedido
+    {
+        public int NumeroPedido;
+        public List<Produto> itens = new List<Produto>();
+
+        public double CalcularValorTotal()
+        {
+            double ValorTotal = 0;
+            foreach (var item in itens)
+            {
+                ValorTotal += item.Preco;
+            }
+            return ValorTotal;
+
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Pedido # " + NumeroPedido);
+            foreach (var item in itens)
+            {
+                Console.WriteLine("Produto: " + item.Nome + " Preço: " + item.Preco);
+            }
+
+            Console.WriteLine($"Total da compra: {CalcularValorTotal()}");
+
+        }
+
+        
+
+    }
+
+}
---
+++ POO-EX1/POO-EX1/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace POO_EX1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto Hamburgue = new Produto();
+            Hamburgue.Nome = "Hamburgue";
+            Hamburgue.Preco = 34.5;
+            Produto Batata = new Produto();
+            Batata.Nome = "Batata-frita";
+            Batata.Preco = 13.5;
+            Produto Refrigerante = new Produto();
+            Refrigerante.Nome = "Refrigerante";
+            Refrigerante.Preco = 5.0;
+            Produto Agua = new Produto();
+            Agua.Nome = "Agua";
+            Agua.Preco = 3.0;
+            Produto Sorvete = new Produto();
+            Sorvete.Nome = "Sorvete";
+            Sorvete.Preco = 7.0;
+            Produto Cerveja = new Produto();
+            Cerveja.Nome = "Cerveja";
+            Cerveja.Preco = 10.0;
+
+
+
+            Pedido pedido = new Pedido();
+            pedido.NumeroPedido = 1;
+            pedido.itens.Add(Hamburgue);            
+            pedido.itens.Add(Batata);
+            pedido.itens.Add(Refrigerante);
+            pedido.itens.Add(Agua);
+            pedido.Print();
+           
+
+
+
+        }
+    }
+}
